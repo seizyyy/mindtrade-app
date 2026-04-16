2249109612965a28dd18b352cce4363115a0a970
keven <seizyv2@gmail.com>
2026-04-16 04:09:51 +0200

    Fix Stripe API version

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2026-03-25.dahlia",
 });
 
 const supabase = createClient(
